b1538147e142b93bed95acc984edc59c7215c229
aktam <aktam36911@gmail.com>
2023-03-01 14:03:01 +0500

    uzbek1

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,22 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+int func(int a, int b, int c) {
+  int answer = 0;
+  if (a < b && a < c) {
+    return answer + b;
+  } else {
+    if (b < a && b < c) {
+      return answer + a;
+    } else {
+      if (c < a && c < b) {
+        return answer + c;
+      }
+    }
+    return answer;
+  }
+}
+
 void main() {
+  print(func(2, 3, 4));
 }
