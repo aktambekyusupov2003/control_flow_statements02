6337d0f67aa2306d06bdfbe45aaa494cc14146c3
aktam <aktam36911@gmail.com>
2023-03-02 12:03:00 +0500

    dart

--- bin/if04.dart
+++ bin/if04.dart
@@ -7,6 +7,18 @@ Args:
 Returns:
     int: return answer. 
 */
+int func(int a, int b) {
+  int answer = 0;
+  if (a == b) {
+    return answer;
+  }
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
 void main() {
+  print(func(5, 5));
 }
